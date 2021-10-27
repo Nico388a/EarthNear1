6ad67e25ae546923a3bce994ed5c5b9f6d885c46
Nicolai <nico388a@edu.easj.dk>
2021-10-27 14:42:16 +0200

    Shift Model

---
+++ EarthNear1/Models/Shift.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EarthNear1.Models
+{
+    public class Shift
+    {
+        public string Description { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+    }
+}
