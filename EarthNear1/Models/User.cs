f6a384c3dd4452fb5df1c3891c222acc1f499360
Arsen Niyonzima <arse0012@edu.easj.dk>
2021-10-29 11:15:16 +0200

    User properties

--- EarthNear1/Models/User.cs
+++ EarthNear1/Models/User.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,11 @@ namespace EarthNear1.Models
 {
     public class User
     {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string AfterName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
