94925dd42a1aff75daed0eac5ef87dd7e576be8e
Nicolai <nico388a@edu.easj.dk>
2021-10-29 11:15:19 +0200

    ExistsException and Database

---
+++ EarthNear1/Exceptions/ExistsException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EarthNear1.Exceptions
+{
+    public class ExistsException: Exception
+    {
+        public ExistsException(string message) : base(message)
+        {
+            
+        }
+    }
+}
